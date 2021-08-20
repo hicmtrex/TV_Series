87cdbf599a24f428759d611a0eec36aa7497d6c7
hicm <hicmrex@gmail.com>
2021-08-20 02:46:58 +0100

    last api

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL = "https://goofy-swartz-ac55cb.netlify.app/"
+    baseURL : "https://goofy-swartz-ac55cb.netlify.app/"
 })
